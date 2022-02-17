22f0385fd44d513180ecdb0e3d83f5a4f20b17e2
Unzor <71789103+Unzor@users.noreply.github.com>
2022-02-16 23:01:20 -0600

    Update index.js

--- examples/socket-io-stream/index.js
+++ examples/socket-io-stream/index.js
@@ -1,6 +1,7 @@
+var isRunning = false;
 var fs = require("fs");
 var index = 0;
-var imageboy = require("imageboy");
+var imageboy = require("./imageboy");
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -16,25 +17,45 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('run', () => {
-      var keysToPress = [];
-        var emulator = imageboy.runDataURL({
-            path: "pokemon_blue.gb",
-            interval: 16.6666667,
-            onFrame: function(frame) {
-                io.emit("frame", frame);
+        if (!isRunning) {
 
-                socket.on('keydown', function(data) {
-                    var index = keysToPress.indexOf(data.key);
-                    if (index === -1) {
-                        keysToPress.push(data.key);
-                    }
-                    emulator.pressKeys(emulator.gameboy, keysToPress);
-                    keysToPress = [];
-                });
-            }
-        })
-    })
-});
+            isRunning = true;
+            global.emulator = imageboy.runDataURL({
+                path: "pokblu.gb",
+                interval: 16.6666667,
+                onFrame: function(frame) {
+                    global.frame = frame;
+                    io.emit("frame", frame);
+
+                    socket.on('keydown', function(data) {
+                        var index = keysToPress.indexOf(data.key);
+                        if (index === -1) {
+                            keysToPress.push(data.key);
+                        }
+                        emulator.pressKeys(emulator.gameboy, keysToPress);
+                        keysToPress = [];
+                    });
+
+                }
+            })
+
+        } else {
+            io.emit("frame", global.frame);
+
+            socket.on('keydown', function(data) {
+                var index = keysToPress.indexOf(data.key);
+                if (index === -1) {
+                    keysToPress.push(data.key);
+                }
+                emulator.pressKeys(emulator.gameboy, keysToPress);
+                keysToPress = [];
+            });
+        }
+    });
+})
+
+
+var keysToPress = [];
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
